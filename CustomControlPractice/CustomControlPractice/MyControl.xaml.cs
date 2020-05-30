c056f2d0246045f3c296149fa097b5665d8c725c
Shuhey <shuhey.dev@gmail.com>
2020-05-30 21:00:29 +0900

    Add project files.

---
+++ CustomControlPractice/CustomControlPractice/MyControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomControlPractice
+{ 
+    [DesignTimeVisible(true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyControl : ContentView
+    {
+        //private string _text;
+        //public string Text
+        //{
+        //    get => _text;
+        //    set
+        //    {
+        //        _text = value;
+        //        this.entry.Text = _text;
+        //    }
+        //}
+
+        public string Text { get; set; }
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+                propertyName: nameof(Text),
+                returnType: typeof(string),
+                declaringType: typeof(MyControl),
+                defaultValue: "",
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanged: TitlePropertyChanged
+            );
+        private static void TitlePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (MyControl)bindable;
+            control.entry.Text = newValue.ToString();
+        }
+
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
